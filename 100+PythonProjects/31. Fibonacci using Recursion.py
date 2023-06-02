55d498e661c4040d5c9d873a86988c07be7509b8
shivinn <shivin7799@gmail.com>
2023-06-02 12:12:06 +0530

    added programs

--- 100+PythonProjects/31. Fibonacci using Recursion.py
+++ 100+PythonProjects/31. Fibonacci using Recursion.py
@@ -1,2 +1,16 @@
 # Python program to display fibonacci sequence using recursion
-# Solution: Go to time 3:15:55
+# Solution: Recursion
+
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2) # 4th term + 3rd term = 5th term
+
+
+n = int(input(" Enter n: "))
+if n <= 0:
+    print(" Enter a positive number!")
+else:
+    for i in range(n):
+        print(fibo(i))
